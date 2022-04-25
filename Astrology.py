c6f68838699a33a4d7dc96aa75bc5a28d30e4243
jlotz <jlotz1998@gmail.com>
2022-04-25 15:40:48 -0400

    first commit

---
+++ Astrology.py
@@ -0,0 +1,84 @@
+month = input("what month were you born?")
+
+if month == "January":
+    day = int(input("What day were you born?"))
+    if day <=19:
+        print("Capricorn")
+    else:
+        print("Aquarius")
+        
+if month == "February":
+    day = int(input("What day were you born?"))
+    if day <= 18:
+        print("Aquarius")
+    else:
+        print("Pices")
+        
+if month == "March":
+    day = int(input("What day were you born?"))
+    if day <= 20:
+        print("Pices")
+    else:
+        print("Aries")
+        
+if month == "April":
+    day = int(input("What day were you born?"))
+    if day <= 19:
+        print("Aries")
+    else:
+        print("Taurus")
+        
+if month == "May":
+    day = int(input("What day were you born?"))
+    if day <= 20:
+        print("Tarus")
+    else:
+        print("Gemini")
+        
+if month == "June":
+    day = int(input("What day were you born?"))
+    if day <= 21:
+        print("Gemini")
+    else:
+        print("Cancer")
+        
+if month == "July":
+    day = int(input("What day were you born?"))
+    if day <= 22:
+        print("Cancer")
+    else:
+        print("Leo")
+if month == "August":
+    day = int(input("What day were you born?"))
+    if day <= 22:
+        print("Leo")
+    else:
+        print("Virgo")
+        
+if month == "September":
+    day = int(input("What day were you born?"))
+    if day <= 22:
+        print("Virgo")
+    else:
+        print("Libra")
+        
+if month == "October":
+    day = input("What day were you born?")
+    if day <= 23:
+        print("Libra")
+    else:
+        print("Scorpio")
+        
+if month == "November":
+    day = int(input("What day were you born?"))
+    if day <= 21:
+        print("Scorpio")
+    else:
+        print("Sagittarius")
+        
+if month == "December":
+    day = int(input("What day were you born?"))
+    if day <= 21:
+        print("Sagittarius")
+    else:
+        print("Capricorn")
